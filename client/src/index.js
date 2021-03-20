29a201296dc99d1b0cc65d4fbff2c9e6577e769e
Ahuoyiza <sophieoyiza@gmail.com>
2021-03-21 00:07:37 +0100

    redux-setup

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,23 @@ import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter} from "react-router-dom";
 
-import 'antd/dist/antd.css'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+import 'antd/dist/antd.css';
+
+//create the store
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   //<React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+  <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </Provider>,
    
   //</React.StrictMode>,
   document.getElementById('root')
